28bd87758202119e9c687a3ea534842f2dd5c3f2
GEWPTHEKILLERSM <paulosergiolobogoes@gmail.com>
2023-10-02 14:46:23 +0000

    Pacote

---
+++ src/NoThread/GerenciadorDeArquivos.java
@@ -0,0 +1,62 @@
+package NoThread;
+
+import javax.swing.filechooser.FileSystemView;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.io.File;
+import java.io.IOException;
+
+public class GerenciadorDeArquivos {
+    private final File desktop = new File(FileSystemView.getFileSystemView().getHomeDirectory().toString());//Área de trabalho
+    private final File folder = new File(desktop, "Calculo 2 Riemann - resultados");//Pasta de arquivos
+    private final File txt = getTxtFile();
+
+    public void createFolder() throws InterruptedException { //Cria a pasta de respostas caso não exista
+        if (!folder.exists()) {
+            boolean created = folder.mkdirs();
+            if (created) {
+                System.out.println("Pasta criada.");
+            } else {
+                System.err.println("Não foi possível criar a pasta.");
+            }
+        } else {
+            System.out.println("A pasta já existe.");
+        }
+        Thread.sleep(1000);
+    }
+
+    private File getTxtFile() { //Cria um novo documento de texto
+        String baseFileName = "resultado.txt";
+        int fileNumber = 0;
+        File txtFile;
+        do {
+            String fileName = baseFileName;
+            if (fileNumber > 0) {
+                fileName = baseFileName.replace(".txt", "(" + fileNumber + ").txt");
+            }
+            txtFile = new File(folder, fileName);
+            fileNumber++;
+        } while (txtFile.exists());
+        return txtFile;
+    }
+
+    public void showText() throws IOException, InterruptedException, AWTException {
+        //Comando para abrir o documento de texto
+        ProcessBuilder pb = new ProcessBuilder("notepad.exe", txt.getAbsolutePath());
+        pb.start();
+
+        Thread.sleep(1000);//Espera 2 segundos para que o documento de texto abra
+
+        Robot robot = new Robot(); //Maximiza a janela
+        robot.keyPress(KeyEvent.VK_WINDOWS);
+        robot.keyPress(KeyEvent.VK_UP);
+        robot.keyRelease(KeyEvent.VK_UP);
+        robot.keyRelease(KeyEvent.VK_WINDOWS);
+
+        System.out.println("Maximizado");
+    }
+
+    public File getTxt() {
+        return txt;
+    }
+}
---
+++ src/NoThread/Main.java
@@ -0,0 +1,41 @@
+package NoThread;
+
+import javax.swing.*;
+import java.awt.*;
+import java.io.IOException;
+import java.util.function.Function;
+
+/*
+* Documento de texto so funciona se notepad.exe tiver os seguintes parâmetros:
+* Fonte: Consolas
+* Estilo: Regular
+* Tamanho: 11
+* Escrita: Ocidental
+* */
+
+public class Main {
+    //Função já definida no código
+    public static double function(double x) {
+        return Math.pow(x, 2);//x^2
+    }
+
+    public static void main(String[] args) throws IOException, InterruptedException, AWTException {
+        Function<Double, Double> function = Main::function; //Recebe a função
+        String s = "x^2";
+
+        /*
+         * a: Limite inferior
+         * b: Limite superior
+         * n: Intervalos
+         * */
+        double a = Double.parseDouble(JOptionPane.showInputDialog("Digite o limite inferior da integral: "));
+        double b = Double.parseDouble(JOptionPane.showInputDialog("Digite o limite superior da integral: "));
+        int n = Integer.parseInt(JOptionPane.showInputDialog("Digite o número de intervalos: "));
+
+        /*
+         * Cria e calcula a soma de Riemann passando os parâmetros
+         * */
+        Riemann r = new Riemann();
+        r.calculate(function, a, b, n, s);
+    }
+}
